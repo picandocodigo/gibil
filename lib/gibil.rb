79137e955b3478fa465b624b466010a8d33653a3
Fernando Briano <fernando@picandocodigo.net>
2024-08-05 19:38:15 +0100

    Updates temperature code and timeout

--- lib/gibil.rb
+++ lib/gibil.rb
@@ -10,12 +10,34 @@ module Gibil
     # == Returns:
     # A float value of the system's temperature
     def self.temperature
+      if File.file?('/sys/class/hwmon/')
+        hwmon
+      elsif !Dir.glob('/sys/class/thermal/thermal_zone*/temp').empty? &&
+            (files = Dir.glob('/sys/class/thermal/thermal_zone*/temp'))
+        thermal(files)
+      else
+        0
+      end
+    end
+
+    def self.hwmon
       Find.find('/sys/class/hwmon/') do |path|
         Dir["#{path}/*"].select do |file|
           return File.read(file).to_f / 1000 if file.match?(/temp[0-9]+_input/)
         end
       end
-      raise "couldn't read temperature"
+    end
+
+    def self.thermal(files)
+      total = 0
+      files.each do |t|
+        n = t.match(/[0-9]+/)[0]
+        tipo = File.read("/sys/class/thermal/thermal_zone#{n}/type").strip
+        temp = File.read(t).to_i / 1000
+        puts "#{tipo}: #{temp}°C"
+        total += temp
+      end
+      total / files.count
     end
   end
 
@@ -27,14 +49,21 @@ module Gibil
 
       n = Libnotify.new(
         summary: 'Temperature',
-        body: "Your computer's temperature is now #{temp} °C",
-        timeout: 1.5,
+        body: message(temp),
+        timeout: 3.0,
         append: true
       )
 
       n.urgency = temp > 80 ? :critical : :normal
-
       n.show!
+    end
+
+    def self.message(temp)
+      if temp.positive?
+        "Your computer's temperature is ~#{temp} °C"
+      else
+        'Couldn\'t read the computer\'s temperature.'
+      end
     end
   end
 
